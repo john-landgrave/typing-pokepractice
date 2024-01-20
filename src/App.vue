80c32218f39d58cc9a42b45b463d0c5e2b79dd27
John Landgrave <john.landgrave@gmail.com>
2024-01-20 20:33:45 +0000

    V1

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,42 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
+import Pokemon from './components/Pokemon.vue';
+import JSConfetti from 'js-confetti'
+
+const pokemonNumber = ref(132);
+const isCorrect = ref(false);
+const confetti = new JSConfetti();
+
+const checkAnswer = (correct: boolean) => {
+  isCorrect.value = correct;
+  if (correct) {
+    confetti.addConfetti();
+  }
+}
+
+const next = () => {
+  if (isCorrect) {
+    const randomNumber = new Uint8Array(1);
+    crypto.getRandomValues(randomNumber);
+    pokemonNumber.value = Math.floor((randomNumber[0]/256) * 152);
+  }
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <Suspense>
+    <div>
+      <Pokemon class="mb-10" :key="pokemonNumber" :no="pokemonNumber" @done="checkAnswer" @next="next"></Pokemon>
+      <div class="flex flex-row justify-center w-[500px]">
+        <button :disabled="!isCorrect" type="button" @click="next">Next</button>
+      </div>
+    </div>
+    <template #fallback>
+      <div>
+        Loading your pokemon...
+      </div>
+    </template>
+  </Suspense>
 </template>
 
 <style scoped>
@@ -26,5 +51,16 @@ import HelloWorld from './components/HelloWorld.vue'
 }
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
+}
+
+button {
+  @apply border rounded-lg border-gray-700;
+}
+button:not(:disabled) {
+  @apply border rounded-lg border-black hover:text-white hover:bg-black transition-colors;
+}
+
+button:disabled {
+  @apply cursor-not-allowed bg-gray-700 text-gray-500;
 }
 </style>
--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -1,5 +1,89 @@
-<template></template>
+<template>
+    <div @click="input?.focus()" class="h-[182px]">
+        <div class="flex flex-row justify-center items-center">
+            <img class="h-[150px]" :src="imageURL">
+            <span @click="sayName" class="volume-icon"><i class="fas fa-volume-up"></i></span>
+        </div>
+        <div class="text-2xl h-[32px]">
+            <span v-for="char in printedAnswer" :class="{'correct': char.isCorrect, 'incorrect': !char.isCorrect}">{{ char.char }}</span>
+        </div>
+        <input class="opacity-0" ref="input" type="text" v-model="rawAnswer" @keyup.enter="emit('next')">
+    </div>
+</template>
 
 <script lang="ts" setup>
-import {} from 'pokeapi-js-wrapper';
+import * as P from 'pokeapi-js-wrapper';
+import { Ref, computed, onMounted, ref, watchEffect } from 'vue';
+
+const props = defineProps<{
+    no: number;
+}>();
+
+const emit = defineEmits<{
+    (e: 'done', isCorrect: boolean): void;
+    (e: 'next'): void;
+}>();
+
+type AnswerCharacter = {
+    char: string;
+    isCorrect: boolean;
+}
+
+const rawAnswer = ref('');
+const input: Ref<HTMLInputElement|null> = ref(null);
+
+const p = new P.Pokedex();
+const pokemon = await p.getPokemonByName(props.no);
+
+const imageURL = pokemon?.sprites.front_default;
+const name = pokemon?.name.toLowerCase();
+
+const s = window.speechSynthesis;
+
+const printedAnswer = computed<AnswerCharacter[]>(() => {
+    return Array.from(rawAnswer.value.toLowerCase()).map((char, index) => {
+        return {
+            char: rawAnswer.value[index],
+            isCorrect: char === name[index],
+        };
+    })
+});
+
+watchEffect(() => {
+    if (printedAnswer.value.length === name.length && printedAnswer.value.reduce((agg, curr) => agg && curr.isCorrect, true)) {
+        emit('done', true);
+    } else {
+        emit('done', false);
+    }
+})
+
+const sayName = () => {
+    const voice = s.getVoices()[0];
+    const utterance = new SpeechSynthesisUtterance(name);
+    utterance.voice = voice;
+    s.speak(utterance);
+}
+
+onMounted(() => {
+    input.value?.focus();
+})
 </script>
+
+<style lang="css" scoped>
+    img {
+        width: 150px;
+        filter: contrast(0) brightness(0%);
+    }
+
+    .volume-icon {
+        font-size: 24px;
+    }
+
+    .correct {
+        color: green;
+    }
+
+    .incorrect {
+        color: red;
+    }
+</style>
